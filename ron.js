b9a9d4a91220ae2da20f3932956dc44b0aa457cf
xavirifa-lgtm <xavi.rifa@googlemail.com>
2026-04-27 13:21:41 +0200

    Add files via upload

--- ron.js
+++ ron.js
@@ -321,7 +321,7 @@ const ronFace = {
                 method: 'POST',
                 headers: { 'Authorization': `Bearer ${this.apiKey}`, 'Content-Type': 'application/json' },
                 body: JSON.stringify({
-                    model: "llama-3.2-90b-vision-preview",
+                    model: "meta-llama/llama-4-scout-17b-16e-instruct",
                     messages: [
                         {
                             role: "user",
